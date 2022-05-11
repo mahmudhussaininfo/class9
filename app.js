21119efc0a55716295486efdd3064ee7da094935
Mahmud <77590197+mahmudhussaininfo@users.noreply.github.com>
2022-05-11 19:19:18 +0600

    Uplad my Assignment no 9

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+// let data = JSON.parse(localStorage.getItem('devs'));
+
+// data.map((data, index) => {
+
+//     console.log(` 
+
+//     name : ${data.name}
+//     Age : ${data.age}
+    
+    
+//     `);
+
+// });
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+let data = JSON.parse(localStorage.getItem('devs'));
+
+// data.map((data, index) => {
+
+//     console.log(` 
+
+//     name : ${data.name}
+//     Age : ${data.age}
+    
+    
+//     `);
+
+// });
